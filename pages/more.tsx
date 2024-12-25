30d2d511e04a1bc279753f600b3ba9c7808edd7a
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-12-25 23:35:47 +0800

    👩‍🎤

--- pages/more.tsx
+++ pages/more.tsx
@@ -7,7 +7,7 @@ import { LinkPreview } from "@/components/ui/link-preview";
 import { NotionPageHeader } from '@/components/NotionPageHeader'
 import { FollowerPointerCard } from "@/components/ui/following-pointer";
 import Image from "next/image";
-
+import { FocusCards } from "@/components/ui/focus-cards";
 export default function Custom() {
   const words = [
     {
@@ -27,6 +27,34 @@ export default function Custom() {
       className: "text-blue-500 dark:text-blue-500",
     },
   ];
+
+  const cards = [
+    {
+      title: "Forest Adventure",
+      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Valley of life",
+      src: "https://images.unsplash.com/photo-1600271772470-bd22a42787b3?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Sala behta hi jayega",
+      src: "https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Camping is for pros",
+      src: "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "The road not taken",
+      src: "https://images.unsplash.com/photo-1507041957456-9c397ce39c97?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "The First Rule",
+      src: "https://images.unsplash.com/photo-1733370111606-14847979fae5?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+
 
   const testimonials = [
     {
@@ -126,7 +154,8 @@ export default function Custom() {
         </p>
 
         <AnimatedTestimonials testimonials={testimonials} />
-
+        <FocusCards cards={cards} />
+        <div className="h-10"></div>
       </div>
 
     </div>
