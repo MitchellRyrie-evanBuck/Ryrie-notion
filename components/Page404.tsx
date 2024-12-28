7b8c11d921f3d86fc79ab335831a257392fe76a7
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-12-29 00:11:15 +0800

    pref: rex

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -2,6 +2,7 @@ import type * as types from '@/lib/types'
 
 import { PageHead } from './PageHead'
 import styles from './styles.module.css'
+import Image from 'next/image'
 
 export function Page404({ site, pageId, error }: types.PageProps) {
   const title = site?.name || 'Notion Page Not Found'
@@ -25,9 +26,12 @@ export function Page404({ site, pageId, error }: types.PageProps) {
             )
           )}
 
-          <img
+          <Image
             src='/404.png'
-            alt='404 Not Found'
+            alt='404'
+            width={500}
+            height={500}
+            priority
             className={styles.errorImage}
           />
         </main>
---
+++ components/ryrie/RyrieComments.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+
+import * as config from '@/lib/config'
+import { useDarkMode } from '@/lib/use-dark-mode'
+
+/**
+ * @ryrie: Added comments system powered by Giscus
+ * TODO: could make this configurable in the future.
+ */
+export const RyrieComments: React.FC = () => {
+  const { isDarkMode } = useDarkMode()
+
+  React.useEffect(() => {
+    const giscusScript = document.createElement('script')
+    giscusScript.src = 'https://giscus.app/client.js'
+    giscusScript.dataset.repo = 'ryrie/me'
+    giscusScript.dataset.repoId = 'R_kgDOId_ODg'
+    giscusScript.dataset.category = 'Posts'
+    giscusScript.dataset.categoryId = 'DIC_kwDOId_ODs4CUre0'
+    giscusScript.dataset.mapping = 'pathname'
+    giscusScript.dataset.strict = '1'
+    giscusScript.dataset.reactionsEnabled = '0'
+    giscusScript.dataset.emitMetadata = '0'
+    giscusScript.dataset.inputPosition = 'top'
+    giscusScript.dataset.theme = `${config.host}/giscus/${isDarkMode ? 'dark' : 'light'}.css`
+    giscusScript.dataset.lang = 'en'
+    giscusScript.dataset.loading = 'lazy'
+    giscusScript.setAttribute('crossorigin', 'anonymous')
+    giscusScript.setAttribute('async', 'true')
+    document.body.append(giscusScript)
+    return () => {
+      giscusScript.remove()
+    }
+  }, [])
+
+  return <div className='giscus'></div>
+}
