32265c0cbd19df617fd3065781ced7faafce9cc0
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-04-23 23:55:38 +0800

    feat: Vercel AI

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Chat from './components/Chat'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-between items-center p-24 min-h-screen">
+      <Chat />
+    </main>
+  )
+}
